520e9812ab1d6262a80d7ad5a2ae247794b45775
applicate2628 <dima_den@inbox.ru>
2026-09-13 12:33:29 +0300

    refactor(provider): drop unused restore adapter

--- internal/api/provider_restore_phase_adapter.go
+++
@@ -1,8 +0,0 @@
-package api
-
-func providerRestorePhaseAndBindingsAllowed(manifestName, phase string) bool {
-	if phase != providerInstallPhaseRecoveryClaimed && phase != providerInstallPhaseManagedSettled {
-		return false
-	}
-	return providerRestoreBindingsClear(manifestName)
-}
